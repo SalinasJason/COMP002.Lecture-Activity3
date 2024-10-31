9d7022765f1408f7e96a62b26d9148d7fc2192f5
SalinasJason <jasonsalinas770@gmail.com>
2024-10-31 14:44:14 -0700

    added answer 2 and 3

--- script.js
+++ script.js
@@ -5,4 +5,24 @@ function greet(name) {
 greet("Jason");
 
 
+function Circumference(diameter) {
+    const pi = 3.14159; 
+    return pi * diameter;
+  }
+  
+  console.log(Circumference(10)); 
+  
+ 
+ 
+function printHelloWorld1() {
+    console.log("This is printHelloWorld1()");
+}
+
+function printHelloWorld2() {
+    console.log("This is printHelloWorld2()");
+    printHelloWorld1();
+}
+
+printHelloWorld2();
+printHelloWorld1();
 
